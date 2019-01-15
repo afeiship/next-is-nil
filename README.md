f0e5776c5a4d52554f5a0dedca8b67034dae63d4
afeiship <afeiship@gmail.com>
2019-01-15 09:05:44 +0800

    feat: init & done

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-is-nil
+> Is null/undefined for next
+
+## install:
+```bash
+npm install -S afeiship/next-is-nil --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ dist/next-is-nil.js
@@ -0,0 +1,16 @@
+(function () {
+
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+
+  nx.isNil = function (inObj) {
+    // no:eqeqeq
+    return inObj == null;
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.isNil;
+  }
+
+}());
