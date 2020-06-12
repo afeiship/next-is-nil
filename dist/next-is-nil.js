a8c776c238e531b56e1c0e129e349568665e35f8
afeiship <1290657123@qq.com>
2020-06-12 08:51:59 +0800

    feat: build dist

--- dist/next-is-nil.js
+++ dist/next-is-nil.js
@@ -1,18 +1,18 @@
 /*!
  * name: @feizheng/next-is-nil
  * description: Is null/undefined for next.
- * url: https://github.com/afeiship/next-is-nil
+ * homepage: https://github.com/afeiship/next-is-nil
  * version: 1.0.0
- * date: 2020-04-15 14:51:22
+ * date: 2020-06-12T00:51:52.083Z
  * license: MIT
  */
 
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.isNil = function() {
-    //code goes here.
+  nx.isNil = function (inTarget) {
+    return inTarget == null;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
